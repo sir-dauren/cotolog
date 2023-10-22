79560f28e4076129f3d60be4ca6f576b1c9b8b9c
sir-dauren <mysqli@list.ru>
2023-10-22 14:25:34 +0600

    Stage2

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -2,14 +2,24 @@ class Products {
 
     render() {
         let htmlCatalog = '';
-        COTOLOG.forEach(({ id, name, price, img }) => {
+        CATALOG.forEach(({ id, name, price, img }) => {
             htmlCatalog += `
-                <li>
-                    <span>${name}</span>
-                    <img src="${img}"/>
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
+                    <img class="products-element__img" src="${img}"/>
+                    <span class="products-element__price">⚡${price.toLocaleString()} KZT </span>
+                    <button class="products-element__btn">Добавить в корзину</button>
                 </li>
             `;
-        })
+        });
+
+        const html = `
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
     }
 }
 
--- constants/catalog.js
+++ constants/catalog.js
@@ -1,61 +1,61 @@
-const COTOLOG = [{
+const CATALOG = [{
         id: 'el1',
         name: 'ARIA GUITARS AFN-15 Black',
-        img: 'img/gitar1.jpg',
+        img: 'img/cotolog/gitar1.jpg',
         price: 40806,
     },
     {
         id: 'el',
         name: 'YAMAHA JR1 NATURAL',
-        img: 'img/gitar2.jpg',
+        img: 'img/cotolog/gitar2.jpg',
         price: 62727,
     },
     {
         id: 'el3',
         name: 'FSX315C TOBACCO BROWN SUNBURST',
-        img: 'img/gitar3.jpg',
+        img: 'img/cotolog/gitar3.jpg',
         price: 104000,
     },
     {
         id: 'el4',
         name: 'ARIA GUITARS ARIA-101CE Matte Natural',
-        img: 'img/gitar4.jpg',
+        img: 'img/cotolog/gitar4.jpg',
         price: 76000,
     },
     {
         id: 'el5',
         name: 'ARIA GUITARS ARIA-111CE Matte Natural',
-        img: 'img/gitar5.jpg',
+        img: 'img/cotolog/gitar5.jpg',
         price: 79000,
     },
     {
         id: 'el6',
         name: 'YAMAHA F370 BLACK',
-        img: 'img/gitar6.jpg',
+        img: 'img/cotolog/gitar6.jpg',
         price: 94000,
     },
     {
         id: 'el7',
         name: 'IBANEZ PC12MH-OPN',
-        img: 'img/gitar7.png',
+        img: 'img/cotolog/gitar7.png',
         price: 104000,
     },
     {
         id: 'el8',
         name: 'YAMAHA FS820 TURQUOISE',
-        img: 'img/gitar8.jpg',
+        img: 'img/cotolog/gitar8.jpg',
         price: 146000,
     },
     {
         id: 'el9',
         name: 'YAMAHA FS820 TURQUOISE',
-        img: 'img/gitar9.jpg',
+        img: 'img/cotolog/gitar9.jpg',
         price: 147000,
     },
     {
         id: 'el10',
         name: 'YAMAHA FG840 NT',
-        img: 'img/gitar10.jpg',
+        img: 'img/cotolog/gitar10.jpg',
         price: 206000,
     }
 ];
