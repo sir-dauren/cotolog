6b884fb2af06c5402a8e6650f627095c6851f1bf
sir-dauren <mysqli@list.ru>
2023-10-22 10:06:34 +0600

    first commit

---
+++ constants/catalog.js
@@ -0,0 +1,19 @@
+const COTOLOG = [{
+        id: 'el1',
+        name: 'ARIA GUITARS AFN-15 Black',
+        img: 'img/gitar1.jpg',
+        price: 40806,
+    },
+    {
+        id: 'el',
+        name: 'YAMAHA JR1 NATURAL',
+        img: 'img/gitar2.jpg',
+        price: 62727,
+    },
+    {
+        id: 'el3',
+        name: 'FSX315C TOBACCO BROWN SUNBURST',
+        img: 'img/gitar3.jpg',
+        price: 104000,
+    },
+];
